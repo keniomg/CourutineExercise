7521dd9ab5fad03b7d41bae808b50d2ef896c4aa
keniomg <keniomg77798@gmail.com>
2024-05-27 21:13:31 +0400

    1

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
     [SerializeField] private float _startValue;
-    [SerializeField] private float _stepValue;
+    [SerializeField] private float _valueStep;
+    [SerializeField] private float _timeStep;
 
     private float _currentValue;
+    private bool _isCount = false;
+    private Coroutine _count;
 
     public float CurrentValue => _currentValue;
 
@@ -16,11 +20,34 @@ public class Counter : MonoBehaviour
     {
         _currentValue = _startValue;
     }
-    
-    public void Count()
+
+    private IEnumerator Count()
     {
-        _currentValue += _stepValue;
-        Changed?.Invoke(_currentValue);
-        Debug.Log(CurrentValue);
+        while (_isCount)
+        {
+            _currentValue += _valueStep;
+            Changed?.Invoke(_currentValue);
+            Debug.Log(CurrentValue);
+
+            yield return new WaitForSeconds(_timeStep);
+        }
+    }
+
+    public void ChangeCountStatus()
+    {
+        _isCount = !_isCount;
+
+        if (_isCount)
+        {
+            _count = StartCoroutine(Count());
+        }
+        else
+        {
+            if (_count != null)
+            {
+                StopCoroutine(_count);
+                _count = null;
+            }
+        }
     }
 }
